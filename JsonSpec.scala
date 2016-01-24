e4663047897d716ea58086ecf36f6ee9a3f5f392
Tobias Johansson <tobias.j.e.johansson@gmail.com>
2016-01-24 12:32:15 +0100

    better syntax, arrays, code structure

--- JsonSpec.scala
+++ JsonSpec.scala
@@ -1,73 +1,112 @@
 
-// === Data ===============================================
+object JsonSpec {
 
-trait Type
-case class Object    ( fields: Field* ) extends Type
-case class NamedType ( name: String ) extends Type
-case class Field     ( name: String, tpe: Type, example: Option[String], req: Req )
-sealed trait Req
-case object Optional extends Req
-case object Required extends Req
+  // === Data ===============================================
 
-// === Syntax =============================================
+  trait Type
+  case class Object    ( fields: Field* ) extends Type
+  case class Array     ( tpe: Type ) extends Type
+  case class NamedType ( name: String, example: Option[String] ) extends Type
+  case class Field     ( name: String, tpe: Type, example: Option[String], doc: Option[String] )
 
-implicit def string2opt(s: String): Option[String] = Option(s)
-implicit class StringExt(name: String) {
-  def is(fields: Field*): Field             = Field(name, Object(fields:_*), None, Optional)
-  def is(tpe: Type): Field                  = Field(name, tpe, None, Optional)
-  def is(tpe: Type, req: Req): Field        = Field(name, tpe, None, req)
-  def is(tpe: Type, example: String): Field = Field(name, tpe, Some(example), Optional)
-  def is(tpe: Type, example: String, req: Req): Field = Field(name, tpe, Some(example), req)
-}
+  // === Syntax =============================================
 
-implicit class FieldExt(field: Field) {
-  def eg(example: Any) = example match {
-    case str: String => field.copy(example = Option(s""""$str""""))
-    case any: Any    => field.copy(example = Option(any.toString))
+  object Type {
+    def apply(name: String) = NamedType(name, None)
   }
-  def required = field.copy(req = Required)
-}
 
+  def obj(fields: Field*) = Object(fields:_*)
+  def arr(tpe: Type) = Array(tpe)
+
+  implicit class StringIsFieldExt(name: String) {
+    def is(fields: Field*): Field = Field(name, Object(fields:_*), None, None)
+    def is(tpe: Type): Field      = Field(name, tpe, None, None)
+  }
+
+  def makeExample(example: Any) = example match {
+    case str: String => Option(s""""$str"""")
+    case any: Any    => Option(any.toString)
+  }
+
+  implicit class TypeEgExt(tpe: NamedType) {
+    def eg(example: Any) = tpe.copy(example = makeExample(example))
+  }
+
+  implicit class FieldEgExt(field: Field) {
+    def eg(example: Any) = field.copy(example = makeExample(example))
+  }
+
+  implicit class FieldDocExt(field: Field) {
+    def doc(doc: String) = field.copy(doc = Option(doc))
+  }
+
+}
 // === Output =============================================
 
-def render(obj: Type, ind: String = ""): String =
-  obj match {
-    case NamedType(name) =>
-      s"/* $name */"
-    case Object(fs @ _*) =>
-      val newInd = ind + "  "
-      val fields = fs map (render(_, newInd)) mkString (",\n")
-      s"{\n$fields\n$ind}"
-  }
-
-def render(field: Field, ind: String): String = {
-  val Field(name, tpe, example, req) = field
-  val lhs = s""""$name":"""
-  val parts = example match {
-    case Some(ex) => Seq(lhs, ex, render(field.tpe, ind))
-    case None     => Seq(lhs, render(field.tpe, ind))
-  }
-  ind + (parts mkString (" "))
+trait Renderer {
+  def render(obj: JsonSpec.Type, ind: String = ""): String
 }
 
+object RenderJson extends Renderer {
+  import JsonSpec._
 
+  def render(obj: Type, ind: String = ""): String =
+    obj match {
+      case NamedType(name, ex) =>
+        ex getOrElse "null"
+      case Array(tpe) =>
+        val inner = render(tpe, ind)
+        s"[ $inner ]"
+      case Object(fs @ _*) =>
+        val newInd = ind + "  "
+        val fields = fs map (render(_, newInd)) mkString (",\n")
+        s"{\n$fields\n$ind}"
+    }
 
+  def render(field: Field, ind: String): String = {
+    val Field(name, tpe, example, doc) = field
+    val lhs = s""""$name":"""
+    val comment = doc map ("/* " + _ + " */")
+    val parts = example match {
+      case Some(ex) => Seq(lhs, ex) ++ comment
+      case None     => Seq(lhs, render(field.tpe, ind)) ++ comment
+    }
+    ind + (parts mkString (" "))
+  }
+}
 // === Example ============================================
 
-val string = NamedType("string")
-val int    = NamedType("int")
-val float  = NamedType("float")
+import JsonSpec._
 
-val myObj = Object (
-  "firstField" is string eg "example value",
-  "thisField"  is int    eg 123,
-  "foo" is (
+val string = Type("string") eg "some string"
+val int    = Type("int")    eg 100
+val float  = Type("float")  eg 1.0
+
+val myObj = obj (
+  "firstField" is string     doc "the first field",
+  "thisField"  is int        doc "this field is numeric",
+  "anInt"      is int eg 123 doc "this field has an example value",
+  "foo" is obj (
     "value" is float  eg 2.0,
     "name"  is string eg "Iain"
-  )
+  ),
+  "bar" is obj (
+    "value" is float  eg 2.0    doc "the value in meters",
+    "name"  is string eg "Iain" doc "name of the person"
+  ),
+  "list" is arr ( string eg "foo" ),
+  "objects" is arr ( obj (
+    "thing" is obj (
+      "name" is string,
+      "food" is string,
+      "age"  is int
+    )
+  ))
 )
 
 val testObj = Object (
-  "bar" is myObj,
+  "bar"     is myObj,
   "another" is string eg "hello"
 )
+
+println(RenderJson.render(testObj))
