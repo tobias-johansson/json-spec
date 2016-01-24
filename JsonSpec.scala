25bf53a919655733f091f1839c6905c965533525
Tobias Johansson <tobias.j.e.johansson@gmail.com>
2016-01-24 23:13:19 +0100

    render as html table

--- JsonSpec.scala
+++ JsonSpec.scala
@@ -3,9 +3,9 @@ object JsonSpec {
 
   // === Data ===============================================
 
-  trait Type
-  case class Object    ( fields: Field* ) extends Type
-  case class Array     ( tpe: Type ) extends Type
+  trait Type { def name: String }
+  case class Object    ( fields: Field* ) extends Type { val name = "{}" }
+  case class Array     ( tpe: Type ) extends Type { val name = "[]" }
   case class NamedType ( name: String, example: Option[String] ) extends Type
   case class Field     ( name: String, tpe: Type, example: Option[String], doc: Option[String] )
 
@@ -44,13 +44,14 @@ object JsonSpec {
 // === Output =============================================
 
 trait Renderer {
-  def render(obj: JsonSpec.Type, ind: String = ""): String
+  def render(obj: JsonSpec.Type): String
 }
 
 object RenderJson extends Renderer {
   import JsonSpec._
 
-  def render(obj: Type, ind: String = ""): String =
+  def render(obj: Type): String = render(obj, "")
+  def render(obj: Type, ind: String): String =
     obj match {
       case NamedType(name, ex) =>
         ex getOrElse "null"
@@ -68,12 +69,44 @@ object RenderJson extends Renderer {
     val lhs = s""""$name":"""
     val comment = doc map ("/* " + _ + " */")
     val parts = example match {
-      case Some(ex) => Seq(lhs, ex) ++ comment
-      case None     => Seq(lhs, render(field.tpe, ind)) ++ comment
+      case Some(ex) => Seq(lhs, ex)
+      case None     => Seq(lhs, render(field.tpe, ind))
     }
     ind + (parts mkString (" "))
   }
 }
+
+object RenderHtml extends Renderer {
+  import JsonSpec._
+
+  def render(obj: Type): String =
+    obj match {
+      case NamedType(name, ex) =>
+        ex getOrElse "null"
+      case Array(tpe) =>
+        "<table><tr>" + typeCell(tpe.name) + exampleCell(render(tpe)) + "</tr></table>"
+      case Object(fs @ _*) =>
+        val fields = fs map (render) mkString ("\n")
+        // s"<table>\n<tr><th>field</th><th>type</th><th>example</th></tr>$fields\n</table>"
+        s"<table>\n$fields\n</table>"
+    }
+
+  def render(field: Field): String = {
+    val Field(name, tpe, example, doc) = field
+    val doccell = docCell(doc getOrElse "")
+    val parts = example match {
+      case Some(ex) => Seq(fieldCell(name), doccell, typeCell(tpe.name), exampleCell(ex))
+      case None     => Seq(fieldCell(name), doccell, typeCell(tpe.name), exampleCell(render(field.tpe)))
+    }
+    "<tr>" + (parts mkString) + "</tr>"
+  }
+
+  def fieldCell(n: String)   = s"""<td class="field">$n</td>"""
+  def typeCell(n: String)    = s"""<td class="type">$n</td>"""
+  def exampleCell(e: String) = s"""<td class="example">$e</td>"""
+  def docCell(d: String)     = s"""<td class="doc">$d</td>"""
+}
+
 // === Example ============================================
 
 import JsonSpec._
@@ -92,16 +125,16 @@ val myObj = obj (
   ),
   "bar" is obj (
     "value" is float  eg 2.0    doc "the value in meters",
-    "name"  is string eg "Iain" doc "name of the person"
+    "name"  is string eg "Paul" doc "name of the person"
   ),
-  "list" is arr ( string eg "foo" ),
+  "list" is arr ( string eg "foo" ) doc "list of strings",
   "objects" is arr ( obj (
     "thing" is obj (
       "name" is string,
       "food" is string,
       "age"  is int
     )
-  ))
+  )) doc "list of objects"
 )
 
 val testObj = Object (
@@ -109,4 +142,5 @@ val testObj = Object (
   "another" is string eg "hello"
 )
 
-println(RenderJson.render(testObj))
+// println(RenderJson.render(testObj))
+println(RenderHtml.render(testObj))
